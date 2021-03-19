c9f3835273d52576e63684648d6ebe05e1082e1d
notbasketball <53055071+notbasketball@users.noreply.github.com>
2021-03-19 14:20:38 -0400

    Add files via upload

--- weather.py
+++ weather.py
@@ -33,24 +33,14 @@ print("36 Hour Forecast:")
 
 # forecasts.0.day = only in day time
 # forecasts.0.night = always
-
-if "daypart_name" in twelve_hour:
-    print(str(twelve_hour["daypart_name"]))
-
 if "day" in twelve_hour:
     print(str(twelve_hour["day"]["daypart_name"]) + ": " + str(twelve_hour["day"]["narrative"]))
-
-print(str(twelve_hour["night"]["daypart_name"]) + ": " + str(twelve_hour["night"]["narrative"]))
-# twelvehour = today or tonight depending on the day, twentyFourhour = tonight or tomorrow depending on the day
-if "day" in twenty_four_hour: 
-    if "day" not in twelve_hour:
-        print(str(twenty_four_hour["day"]["daypart_name"]) + ": " + str(twenty_four_hour["day"]["narrative"]))
-    print(str(twenty_four_hour["night"]["daypart_name"]) + ": " + str(twenty_four_hour["night"]["narrative"]))
+    print(str(twelve_hour["night"]["daypart_name"]) + ": " + str(twelve_hour["night"]["narrative"]))
+    print(str(twenty_four_hour["day"]["daypart_name"]) + ": " + str(twenty_four_hour["day"]["narrative"]))
 else:
+    print(str(twelve_hour["night"]["daypart_name"]) + ": " + str(twelve_hour["night"]["narrative"]))
     print(str(twenty_four_hour["day"]["daypart_name"]) + ": " + str(twenty_four_hour["day"]["narrative"]))
     print(str(twenty_four_hour["night"]["daypart_name"]) + ": " + str(twenty_four_hour["night"]["narrative"]))
-# 4pm tonight, tomorrow, tomorrowNight 
-# line 45 = 8am today, tonight, Tomorrow
 print("")
 print(" 5 Day Forecast:")
 for days in range(0, 6):
@@ -62,4 +52,3 @@ for days in range(0, 6):
             str(five_day[days]["day"]["temp"]) +
             "°F"
         )
-        # test
