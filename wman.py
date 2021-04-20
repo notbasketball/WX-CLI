33246287be06d7927b3e3940d0431bd1a770c26e
notbasketball <53055071+notbasketball@users.noreply.github.com>
2021-04-20 18:27:26 -0400

    Add files via upload

--- wman.py
+++ wman.py
@@ -7,17 +7,20 @@ class WeatherMan:
     def __init__(self):
         self.apikey = None
         self.params = {
-            'lang': 'en-US',
+            'language': 'en-US',
             'units': 'e'
         }
 
-    def get_request(self, url, params = { }):
+    def get_request(self, url, params = { } , blacklist = ()):
         # send api key
         params['apiKey'] = self.apikey
+        params['language'] = self.params['language']
+        params['units'] = self.params['units']
+        for x in blacklist:
+            params.pop(x, None)
 
         # add url params
         url_full = self.base_url + url + '?' + urllib.parse.urlencode(params)
-
         # the http request itself
         req = urllib.request.Request(url_full)
 
@@ -28,8 +31,8 @@ class WeatherMan:
 
         return data
 
-    def get_request_json(self, url, params = { }):
-        return json.loads(self.get_request(url, params))
+    def get_request_json(self, url, params = { }, blacklist = ()):
+        return json.loads(self.get_request(url, params, blacklist))
 
     def set_key(self, key):
         self.apikey = key
@@ -40,23 +43,21 @@ class WeatherMan:
     def set_units(self, units):
         self.params.units = units
 
-    def get_currents_on_demand(self, lat, lon, params = { }):
+    def get_currents_on_demand(self, lat, lon):
         return self.get_request_json('v2/turbo/vt1observation', {
-            'apiKey': self.apikey,
             'geocode': str(lat) + ',' + str(lon),
-            'language': self.params['lang'],
-            'units': self.params['units'],
             'format': 'json'
-        })
-    def get_hourly(self, lat, lon, params = { }):
-    	return self.get_request_json('v1/geocode/{0}/{1}/forecast/hourly/6hour.json'.format(lat, lon), {
-    	'apiKey': self.apikey, 
-    	'language': self.params['lang'],
-    	'units': self.params['units']
-    	})
-    def get_five_day(self, lat, lon, params = { }):
-        return self.get_request_json('v1/geocode/{0}/{1}/forecast/daily/5day.json'.format(lat, lon), {
-            'apiKey': self.apikey,
-            'language': self.params['lang'],
-            'units': self.params['units']
-        })
+        }, ())
+    def get_hourly(self, lat, lon):
+        return self.get_request_json('v1/geocode/{0}/{1}/forecast/hourly/6hour.json'.format(lat, lon), { }, ())
+
+    def get_five_day(self, lat, lon):
+        return self.get_request_json('v1/geocode/{0}/{1}/forecast/daily/5day.json'.format(lat, lon), { }, ())
+    def alerts(self, lat, lon):
+        try:
+            return self.get_request_json('v3/alerts/headlines'.format(lat, lon), {
+                'geocode': str(lat) + ',' + str(lon),
+                'format': 'json'
+            }, ('units', ))
+        except:
+            return None
